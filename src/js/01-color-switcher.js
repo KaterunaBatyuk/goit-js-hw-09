c350bce29a1cc3557d6debbec26d7c2910071e9e
KaterunaBatyuk <katerina.batuk89@gmail.com>
2022-10-27 19:55:03 +0300

    1

---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,12 @@
+const btnStart = document.querySelector('[data-start]');
+const btnStop = document.querySelector('[data-stop]');
+
+console.log(btnStart);
+console.log(btnStop);
+
+btnStart.addEventListener('click', onBtnStartClick);
+btnStop.addEventListener('click', onBtnStopClick);
+
+function onBtnStartClick() {}
+
+function onBtnStopClick() {}
