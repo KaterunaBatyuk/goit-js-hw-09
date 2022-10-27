75d734ed714d620f86fd8a7f4be3e2e8cd39815e
KaterunaBatyuk <104945220+KaterunaBatyuk@users.noreply.github.com>
2022-10-27 22:20:48 +0000

    Deploying to gh-pages from @ KaterunaBatyuk/goit-js-hw-09@59b9e7c7001940c51fca8b496798afbf6458e987 🚀

--- 01-color-switcher.1b4f24ed.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.querySelector("[data-start]"),t=document.querySelector("[data-stop]");console.log(e),console.log(t),e.addEventListener("click",(function(){})),t.addEventListener("click",(function(){}))}();
-//# sourceMappingURL=01-color-switcher.1b4f24ed.js.map
--- 01-color-switcher.c8b904ab.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector("[data-start]"),t=document.querySelector("[data-stop]");console.log(e),console.log(t),e.addEventListener("click",(function(){})),t.addEventListener("click",(function(){}));
-//# sourceMappingURL=01-color-switcher.c8b904ab.js.map
