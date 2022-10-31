43655c6c3d2f7697a0ddbcbf6526d43751745a2f
KaterunaBatyuk <katerina.batuk89@gmail.com>
2022-10-31 10:15:25 +0200

    Завдання виконані

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,39 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+import Notiflix from 'notiflix';
+
+const form = document.querySelector('.form');
+const delay = document.querySelector('[name=delay]');
+const step = document.querySelector('[name=step]');
+const amount = document.querySelector('[name=amount]');
+const create = document.querySelector('button');
+
+form.addEventListener('submit', onFormSub);
+
+function onFormSub(event) {
+  event.preventDefault();
+  for (let i = 0; i < amount.value; i += 1) {
+    createPromise(i + 1, delay + step * i)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(
+          `✅ Fulfilled promise ${position} in ${delay}ms`
+        );
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(
+          `❌ Rejected promise ${position} in ${delay}ms`
+        );
+      });
   }
+}
+
+function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
